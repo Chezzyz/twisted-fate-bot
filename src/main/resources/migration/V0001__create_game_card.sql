7b36c3d61b96d396cb01d1c0f4a82268a0205cbf
Ivan Kolyganov <ivan.kolyganov2302@gmail.com>
2023-12-10 17:24:14 +0500

    data types fixed in migration

--- src/main/resources/migration/V0001__create_game_card.sql
+++ src/main/resources/migration/V0001__create_game_card.sql
@@ -1,12 +1,12 @@
 DROP TABLE IF EXISTS game_cards;
 CREATE TABLE game_cards
 (
-    id               serial      NOT NULL,
-    rus_name          text        NOT NULL,
-    eng_name          text        NOT NULL,
-    image_path        text        NOT NULL,
-    card_value        text        NOT NULL,
-    description      text        NOT NULL,
+    id               serial         NOT NULL,
+    rus_name         varchar(255)   NOT NULL,
+    eng_name         varchar(255)   NOT NULL,
+    image_path       varchar(255)   NOT NULL,
+    card_value       varchar(8)     NOT NULL,
+    description      text           NOT NULL,
     PRIMARY KEY (id),
     UNIQUE (id)
 );
