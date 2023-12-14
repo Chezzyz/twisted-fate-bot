a1249c00451d8419e000459ff35cd0c86d70d893
Xisomba <ekimovskih.DIM@yandex.ru>
2023-12-14 16:48:05 +0500

    data added
    Добавлены  описания и картинки .

--- src/main/java/ru/readysetcock/fate_telegram_bot/model/domain/EnergyStone.java
+++ src/main/java/ru/readysetcock/fate_telegram_bot/model/domain/EnergyStone.java
@@ -14,12 +14,16 @@ public class EnergyStone extends BaseCatalogueDomain {
     @Column(name = "esoteric_description", nullable = false)
     private String esotericDescription;
 
+    @Column(name = "features", nullable = false)
+    private String features;
+
     @Column(name = "real_description", nullable = false)
     private String realDescription;
 
-    public EnergyStone(String rusName, String engName, String imageFileId, String esotericDescription, String realDescription) {
+    public EnergyStone(String rusName, String engName, String imageFileId, String features, String realDescription, String esotericDescription) {
         super(rusName, engName, imageFileId);
         this.esotericDescription = esotericDescription;
+        this.features = features;
         this.realDescription = realDescription;
     }
 }
