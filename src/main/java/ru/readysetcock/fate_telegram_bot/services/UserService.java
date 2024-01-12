e5a18ec14ef1619d01dd940359b6426e421a4450
slaymik <slava.arapoff@gmail.com>
2024-01-12 08:53:11 +0500

    Жестко подумал и убрал костыль в UserService :sweat_smile:

--- src/main/java/ru/readysetcock/fate_telegram_bot/services/UserService.java
+++ src/main/java/ru/readysetcock/fate_telegram_bot/services/UserService.java
@@ -2,6 +2,7 @@ package ru.readysetcock.fate_telegram_bot.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.readysetcock.fate_telegram_bot.model.domain.User;
@@ -15,10 +16,8 @@ public class UserService {
     private final UserRepository repository;
 
     public void userUpdateService(Update update) {
-        if (update.getMessage() != null) {
-            Message message = update.getMessage();
-            org.telegram.telegrambots.meta.api.objects.User user = message.getFrom();
-            repository.save(new User(user.getId(), message.getChatId(), user.getFirstName(), user.getLastName(), user.getUserName()));
+            Message message = update.getCallbackQuery().getMessage();
+            Chat chat = message.getChat();
+            repository.save(new User(chat.getId(), message.getChatId(), chat.getFirstName(), chat.getLastName(), chat.getUserName()));
         }
     }
-}
